13c772494f2d8c1ab01c4a0e7d90136f9edf99d1
vishal gangde <gangdevishal222@gmail.com>
2022-02-02 20:52:10 +0530

    Resolving node-sass problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+
+const Hats = () => {
+  return <div>Hello HATS</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<Hats />} />
+      </Routes>
     </div>
   );
 }
