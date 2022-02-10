ec7b165f7fef1b9b4e25643c39a69719ad323782
vishal gangde <gangdevishal222@gmail.com>
2022-02-10 13:00:24 +0530

    added redux into project and implemented userReducer and userAction in header and app component

--- src/App.js
+++ src/App.js
@@ -7,34 +7,25 @@ import ShopPage from "./pages/shoppage/shop-page.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.util";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.action";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null,
-    };
-  }
+class App extends Component {
   unsubscribeFromAuth = null;
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const useRef = await createUserProfileDocument(userAuth);
 
         useRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
-            },
-            () => console.log(this.state)
-          );
+          setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data(),
+            });
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
 
       // createUserProfileDocument(user);
@@ -46,10 +37,9 @@ export default class App extends Component {
   }
 
   render() {
-    const { currentUser } = this.state;
     return (
       <div>
-        <Header currentUser={currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -59,3 +49,9 @@ export default class App extends Component {
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./root.reducer";
+
+const middlewears = [logger];
+
+const store = createStore(rootReducer,applyMiddleware(...middlewears));
+
+export default store; 
